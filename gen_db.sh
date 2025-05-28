a7bbbfd19cf0d949d178e7f9c56fc571fe939b47
User X955 <x955@www2394.sakura.ne.jp>
2025-05-29 06:24:00 +0900

    20250529

--- gen_db.sh
+++ gen_db.sh
@@ -1,44 +1,51 @@
 #!/usr/local/bin/bash
 
-FILE="./macraw/oui.csv"
+OUTDIR="/home/x955/www/nwapp/macal/"
 
+
+
+FILE="./oui.csv"
 while read line || [ -n "${line}" ];
 do
     tests=(${line//,/ })
 
     vcode="${tests[1]}"
-    ofile="./macdb/${vcode}.txt"
+    ofile="../macdb/${vcode}.txt"
     echo $line > $ofile
 done < "$FILE" 
 
-FILE="./macraw/oui36.csv"
+FILE="./oui36.csv"
 
 while read line || [ -n "${line}" ];
 do
     tests=(${line//,/ })
 
     vcode="${tests[1]}"
-    ofile="./macdb/${vcode}.txt"
+    ofile="../macdb/${vcode}.txt"
 done < "$FILE"
 
-FILE="./macraw/cid.csv"
+FILE="./cid.csv"
 
 while read line || [ -n "${line}" ];
 do
     tests=(${line//,/ })
 
     vcode="${tests[1]}"
-    ofile="./macdb/${vcode}.txt"
+    ofile="../macdb//${vcode}.txt"
     echo $line > $ofile
 done < "$FILE"
 
-FILE="./macraw/mam.csv"
+FILE="./mam.csv"
 
 while read line || [ -n "${line}" ];
 do
     tests=(${line//,/ })
 
     vcode="${tests[1]}"
-    ofile="./macdb/${vcode}.txt"
+    ofile="../macdb/${vcode}.txt"
     echo $line > $ofile
 done < "$FILE"
+
+
+TIMESTAMP=`date '+%Y%m%d%H%M.%S'`
+touch -t ${TIMESTAMP} "macdb"

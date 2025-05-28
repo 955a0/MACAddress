7249772919fec409f7413efbc14896f5bd7efc8d
User X955 <x955@www2394.sakura.ne.jp>
2025-05-28 21:37:58 +0900

    0

---
+++ get.sh
@@ -0,0 +1,10 @@
+!/usr/local/bin/bash
+
+curl -O https://standards-oui.ieee.org/oui/oui.csv
+curl -O https://standards-oui.ieee.org/oui28/mam.csv
+curl -O https://standards-oui.ieee.org/oui36/oui36.csv
+curl -O https://standards-oui.ieee.org/cid/cid.csv
+
+
+#git add *.csv
+#git commit -m "comitt"
